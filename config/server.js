7404c503e6d8cab84ab6f53a0698f4384ed5837d
scottish-ep <scottish.foldep@gmail.com>
2022-12-02 11:02:24 +0700

    push code

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ const cronTasks = require("./cron-task");
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: 1400,
   url: env("PUBLIC_URL", "https://vanlang.eastplayers-client.com"),
   proxy: true,
   app: {
